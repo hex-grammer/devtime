07f4c099451e537e3eb6d3d5e35f21cd0103440e
Rizal Iswandy <=>
2023-08-01 15:08:58 +0800

    setup database & clerk

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { UserButton } from "@clerk/nextjs";
 import Head from "next/head";
 import Link from "next/link";
 import { api } from "~/utils/api";
@@ -17,6 +18,7 @@ export default function Home() {
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
+          <UserButton afterSignOutUrl="/" />
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
