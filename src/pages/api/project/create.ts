215ea6d5b984476fea1db5c86d6b9cf3133be72f
Rizal Iswandy <=>
2023-08-05 15:53:15 +0800

    bug fix

--- src/pages/api/project/create.ts
+++ src/pages/api/project/create.ts
@@ -1,11 +1,11 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
-import { PrismaClient, type Project } from "@prisma/client";
+import { PrismaClient, type project } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Project | { error: string }>
+  res: NextApiResponse<project | { error: string }>
 ) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method Not Allowed" });
